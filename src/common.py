698be0467432a60b09b33b6da9f3618e92e425e3
Samuel Gómez <samuel.gomez.balderas@gmail.com>
2025-01-16 11:50:48 -0600

    refactor on common read_stdin_lines

--- src/common.py
+++ src/common.py
@@ -3,6 +3,7 @@ def read_stdin_lines():
     try:
         while True:
             line = input()
+            if not line: break
             yield line
     except EOFError:
         return
@@ -12,9 +13,6 @@ def prepare_day01_input_data():
     right_list = list()
 
     for line in read_stdin_lines():
-        if not line:
-            break
-        
         left, right = line.split()
         left_list.append(int(left))
         right_list.append(int(right))
@@ -25,9 +23,6 @@ def prepare_day02_input_data():
     report_list = list()
 
     for line in read_stdin_lines():
-        if not line:
-            break
-        
         report = list(map(int, line.split()))
         report_list.append(report)
     
