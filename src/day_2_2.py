d72170be1bf30916a14393a1ef3ce55670bffe7e
Samuel Gómez <samuel.gomez.balderas@gmail.com>
2025-01-16 11:52:38 -0600

    Day 2-2 red-nosed reports

---
+++ src/day_2_2.py
@@ -0,0 +1,31 @@
+from common import prepare_day02_input_data
+from day_2_1 import check_all_increasing_safe, check_all_decreasing_safe
+
+def check_if_removing(report: list):
+    aux_report = report.copy()
+    index = 0
+    while index < len(report):
+        del aux_report[index]
+        if check_all_increasing_safe(aux_report) or check_all_decreasing_safe(aux_report):
+            return True
+        aux_report = report.copy()
+        index += 1
+
+    return False
+
+def red_nosed_reports(report_list: list) -> int:
+    safe_reports = 0
+    for report in report_list:
+        if (check_all_increasing_safe(report) 
+            or check_all_decreasing_safe(report)
+            or check_if_removing(report)):
+            safe_reports += 1
+    
+    return safe_reports
+
+def main():
+    report_list = prepare_day02_input_data()
+    print(red_nosed_reports(report_list))
+
+if __name__ == '__main__':
+    main()
---
+++ tests/test_day_2_2.py
@@ -0,0 +1,9 @@
+import pytest
+from day_2_2 import check_if_removing
+
+def test_check_if_removing():
+    single_report = [8, 6, 4, 4, 1]
+    assert check_if_removing(single_report) == True
+
+def test_red_nosed_reports():
+    assert False
